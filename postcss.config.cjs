4dbdc1b23657b92a760ab02da8a0d443ddcceb11
Emmanuel Beaudry Marchand <emmbema@gmail.com>
2021-11-18 02:02:08 -0500

    adjusting postcss setup for design system vars handling

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -4,9 +4,10 @@ module.exports = {
 		require('postcss-functions')({
 			functions: require('./src/styles/functions.cjs')
 		}),
+		require('postcss-math'),
 		require('postcss-nested'),
-		// require('postcss-each'),
-		// require('postcss-for'),
+		require('postcss-each'),
+		require('postcss-for'),
 		require('autoprefixer'),
 		process.env.NODE_ENV === 'production' && require('cssnano'),
 	]
--- src/styles/functions.cjs
+++ src/styles/functions.cjs
@@ -1,31 +1,48 @@
-exports.get = function(varName) {
+/**
+ * Helper to invoke CSS custom properties without having to prepend double-dash
+ * @param varName String corresponding to the CSS custom property's name without the leading double-dash
+ * @returns CSS Custom property `var(--[...])` call
+ */
+exports.get = (varName) => {
 	return `var(--${varName})`;
 }
 
 
-exports.font = function(fontLevel) {
+exports.font = (fontLevel) => {
 	return `var(--font-${fontLevel})`;
 }
 
 
 /**
- * Takes an unflagged css custom property (i.e.: name without the leading double-dash) holding rgb values
- * and returns the corresponding appropriate rgb declaration.
- * @param colorName String corresponding to the css custom property, without the leading dashes
- * @returns String of format `rgb(var(--[colorName]))`
+ * @param colorString HEX or RGB string defining a color
+ * @returns A string of the color decomposed into its R, G, and B components
  */
-exports.color = function (colorName) {
-	return `rgb(var(--${colorName}))`;
+exports.decompose = (colorString) => {
+	/* rgb */
+	const rgb = colorString.match(/^rgb\(\s*(\d+)\s*,\s*(\d+)\s*,\s*(\d+)\s*\)$/i);
+	if (rgb) {
+		return rgb.slice(1).join(', ');
+	}
+	/* hex */
+	const normal = colorString.match(/^#([0-9a-f]{2})([0-9a-f]{2})([0-9a-f]{2})$/i);
+	if (normal) {
+		return normal.slice(1).map(e => parseInt(e, 16)).join(', ');
+	}
+	const shorthand = colorString.match(/^#([0-9a-f])([0-9a-f])([0-9a-f])$/i);
+	if (shorthand) {
+		return shorthand.slice(1).map(e => 0x11 * parseInt(e, 16)).join(', ');
+	}
+	throw new Error('Provided color string does not match valid formats');
 }
 
 
 /**
  * Takes an unflagged css custom property (i.e.: name without the leading double-dash) holding rgb values
- * and returns the corresponding appropriate rgba declaration, thus enabling the use of alpha with theme colors.
+ * and returns the corresponding appropriate rgb(a) declaration, enabling the use of alpha with theme colors.
  * @param colorName String corresponding to the css custom property, without the leading dashes
  * @param alpha Float (0 to 1) or percentage string (with percent symbol) corresponding to the desired alpha level
  * @returns String of format `rgba(var(--[colorName]), [alpha])`
  */
-exports.fade = function(colorName, alpha) {
-	return `rgba(var(--${colorName}), ${alpha})`;
+exports.color = (colorName, alpha) => {
+	return `rgb(var(--${colorName})${alpha === undefined ? '' : ', ' + alpha})`;
 }
