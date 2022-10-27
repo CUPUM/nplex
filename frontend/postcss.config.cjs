4e23fcb80ff08833a387ddd0e7febc42457f3402
Emmanuel <emmbema@gmail.com>
2022-10-26 23:18:17 -0400

    Init revert to sass and cleanup deps

--- frontend/postcss.config.cjs
+++ frontend/postcss.config.cjs
@@ -1,12 +1,5 @@
 module.exports = {
 	plugins: [
-		// require('postcss-nesting'),
-		// require('postcss-nested'),
-		// require('postcss-font-magician')({
-		// 	hosted: ['./static/fonts/outfit/web'],
-		// 	display: 'swap',
-		// 	formats: 'woff woff2 otf ttf local eot',
-		// }),
 		require('postcss-normalize'),
 		require('autoprefixer'),
 		process.env.NODE_ENV === 'production' && require('cssnano'),
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,15 +1,14 @@
 // @ts-ignore
 import { sveltekit } from '@sveltejs/kit/vite';
-import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
 import type { UserConfig } from 'vite';
 import icons from './plugins/icons';
-// import styles from './plugins/styles';
+import styles from './plugins/styles';
 
 const config: UserConfig = {
 	server: {
 		port: process.env.PORT ? +process.env.PORT : 3000,
 	},
-	plugins: [icons(), vanillaExtractPlugin(), sveltekit()],
+	plugins: [icons(), styles(), sveltekit()],
 	define: {
 		__VITE_DEV_APP_VERSION__: JSON.stringify(Date.now().toString()),
 	},
