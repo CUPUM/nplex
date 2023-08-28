b2e42c8a32610d0613a3f9eb161d0ee429c1ee24
Emmanuel <emmbema@gmail.com>
2023-08-28 11:08:02 -0400

    Update seed script

--- scripts/seed.ts
+++ scripts/seed.ts
@@ -1,9 +1,9 @@
 import { exit } from 'process';
-import { USER_ROLES_ARR } from '../src/lib/auth/constants';
-import { userRoles } from '../src/lib/db/schema/auth';
+import { USER_ROLES, USER_ROLES_ARR } from '../src/lib/auth/constants';
+import { userRoles, userRolesTranslations } from '../src/lib/db/schema/auth';
 import { locales } from '../src/lib/db/schema/i18n';
 import { projectTypes, projectTypesTranslations } from '../src/lib/db/schema/projects';
-import { LOCALES_ARR, LOCALES_DETAILS, type Locale } from '../src/lib/i18n/constants';
+import { LOCALES, LOCALES_ARR, LOCALES_DETAILS, type Locale } from '../src/lib/i18n/constants';
 import { createDrizzle } from './common';
 
 const db = createDrizzle();
@@ -20,67 +20,122 @@ try {
 	seeded.push(ls);
 
 	// Seed user roles
-	const rs = await db
-		.insert(userRoles)
-		.values(USER_ROLES_ARR.map((role) => ({ role })))
-		.onConflictDoNothing()
-		.returning();
-	seeded.push(rs);
-
-	// Seed project types
-	const pt_seed = [
-		{
-			fr: {
-				title: 'Nouvelle construction',
-				description:
-					'Cette catégorie de projet fait référence aux nouveaux projets qui ne s’appuient pas - ou s’appuient très modestement - sur une installation existante.',
-			},
-			en: {
-				title: 'New construction',
-				description: '',
-			},
-		},
-		{
-			fr: {
-				title: 'Transformation',
-				description:
-					'Un projet de transformation consiste en une intervention sur une construction ou un aménagement existant. Cela implique des travaux de rénovation, de valorisation de restauration, etc.',
-			},
-			en: {
-				title: 'Transformation',
-				description: '',
-			},
-		},
-		{
-			fr: {
-				title: 'Restauration',
-				description:
-					'Travaux de restaurations qui s’inscrivent dans une approche de préservation du patrimoine.',
-			},
-			en: {
-				title: 'Restoration',
-				description: '',
-			},
-		},
-	] satisfies Record<Locale, { title: string; description: string }>[];
-	const pt = await db.transaction(async (tx) => {
-		await tx.insert(projectTypes).values(pt_seed.map((v, i) => ({ id: i })));
-		await tx.insert(projectTypesTranslations).values(
-			pt_seed.flatMap((v, i) => {
-				return LOCALES_ARR.map((locale) => {
-					return {
-						id: i,
-						locale,
-						title: v[locale].title,
-						description: v[locale].description,
-					};
-				});
-			})
+	await db.transaction(async (tx) => {
+		seeded.push(
+			await tx
+				.insert(userRoles)
+				.values(USER_ROLES_ARR.map((role) => ({ role })))
+				.onConflictDoNothing()
+				.returning()
+		);
+		seeded.push(
+			await tx
+				.insert(userRolesTranslations)
+				.values([
+					{
+						role: USER_ROLES.VISITOR,
+						locale: LOCALES.FRENCH,
+						name: 'Visiteur',
+						description: 'Rôle de base pour tout utilisateur avec un compte.',
+					},
+					{
+						role: USER_ROLES.VISITOR,
+						locale: LOCALES.ENGLISH,
+						name: 'Visitor',
+						description: 'Default role for every registered user.',
+					},
+					{
+						role: USER_ROLES.EDITOR,
+						locale: LOCALES.FRENCH,
+						name: 'Éditeur',
+						description:
+							'Collaborateur ayant les droits de base pour la gestion et la publication de contenu.',
+					},
+					{
+						role: USER_ROLES.EDITOR,
+						locale: LOCALES.ENGLISH,
+						name: 'Editor',
+						description: '',
+					},
+					{
+						role: USER_ROLES.ADMIN,
+						locale: LOCALES.FRENCH,
+						name: 'Administrateur',
+						description:
+							'Administrateur avec accès et permissions permettant de gérer les droits des éditeurs et visiteurs.',
+					},
+					{
+						role: USER_ROLES.ADMIN,
+						locale: LOCALES.ENGLISH,
+						name: 'Administrator',
+						description: '',
+					},
+				])
+				.onConflictDoNothing()
 		);
 	});
-	seeded.push(pt);
 
-	// Seed ...
+	// Seed project types
+	await db.transaction(async (tx) => {
+		const pt_t = [
+			{
+				fr: {
+					title: 'Nouvelle construction',
+					description:
+						'Cette catégorie de projet fait référence aux nouveaux projets qui ne s’appuient pas - ou s’appuient très modestement - sur une installation existante.',
+				},
+				en: {
+					title: 'New construction',
+					description: '',
+				},
+			},
+			{
+				fr: {
+					title: 'Transformation',
+					description:
+						'Un projet de transformation consiste en une intervention sur une construction ou un aménagement existant. Cela implique des travaux de rénovation, de valorisation de restauration, etc.',
+				},
+				en: {
+					title: 'Transformation',
+					description: '',
+				},
+			},
+			{
+				fr: {
+					title: 'Restauration',
+					description:
+						'Travaux de restaurations qui s’inscrivent dans une approche de préservation du patrimoine.',
+				},
+				en: {
+					title: 'Restoration',
+					description: '',
+				},
+			},
+		] satisfies Record<Locale, { title: string; description: string }>[];
+		seeded.push(
+			await tx
+				.insert(projectTypes)
+				.values(pt_t.map((v, i) => ({ id: i })))
+				.onConflictDoNothing()
+		);
+		seeded.push(
+			await tx
+				.insert(projectTypesTranslations)
+				.values(
+					pt_t.flatMap((v, i) => {
+						return LOCALES_ARR.map((locale) => {
+							return {
+								id: i,
+								locale,
+								title: v[locale].title,
+								description: v[locale].description,
+							};
+						});
+					})
+				)
+				.onConflictDoNothing()
+		);
+	});
 } catch (error) {
 	console.error('❌ Database seed failed (see error below).');
 	throw error;
