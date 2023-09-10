109912092c8a27790cf97ea8b120b888ebce2db4
Emmanuel <emmbema@gmail.com>
2023-09-10 12:27:15 +0300

    Update logo

--- src/lib/components/LogoSvg.svelte
+++ src/lib/components/LogoSvg.svelte
@@ -13,33 +13,32 @@
 	export let mono: $$Props['mono'] = false;
 
 	$: viewBox = mono ? '0 50 100 100' : '0 0 530 200';
-	$: width = mono ? '100' : '530';
 </script>
 
 <svg xmlns="http://www.w3.org/2000/svg" {viewBox} height={size} fill="none" {...$$restProps}>
 	<path
 		id="logo-n"
-		d="M0 100C0 72.3858 22.3858 50 50 50V50C77.6142 50 100 72.3858 100 100V150H0V100Z"
+		d="M0 100C0 72.3858 22.3858 50 50 50C77.6142 50 100 72.3858 100 100V150H0V100Z"
 		fill="currentColor"
 	/>
 	{#if !mono}
 		<g id="logo-p">
+			<path id="logo-p-descendant" d="M120 150H170V200H120V150Z" fill="currentColor" />
 			<path
-				id="logo-p-1"
-				d="M220 100C220 127.614 197.614 150 170 150C142.386 150 120 127.614 120 100C120 72.3858 142.386 50 170 50C197.614 50 220 72.3858 220 100Z"
+				id="logo-p-eye"
+				d="M230 100C230 127.614 207.614 150 180 150C152.386 150 130 127.614 130 100C130 72.3858 152.386 50 180 50C207.614 50 230 72.3858 230 100Z"
 				fill="currentColor"
 			/>
-			<path id="logo-p-2" d="M120 150H170V200H120V150Z" fill="currentColor" />
 		</g>
-		<path id="logo-l" d="M240 0H290V150H240V0Z" fill="currentColor" />
+		<path id="logo-l" d="M250 0H300V150H250V0Z" fill="currentColor" />
 		<path
 			id="logo-e"
-			d="M310 100C310 72.3858 332.386 50 360 50V50C387.614 50 410 72.3858 410 100V100H360V150V150C332.386 150 310 127.614 310 100V100Z"
+			d="M320 100C320 72.3858 342.386 50 370 50C397.614 50 420 72.3858 420 100H370V150C342.386 150 320 127.614 320 100Z"
 			fill="currentColor"
 		/>
 		<g id="logo-x">
-			<path id="logo-x-1" d="M430 100H480V150H430V100Z" fill="currentColor" />
-			<path id="logo-x-2" d="M480 50H530V100H480V50Z" fill="currentColor" />
+			<path id="logo-x-top" d="M530 100L480 100L480 50L530 50L530 100Z" fill="currentColor" />
+			<path id="logo-x-bottom" d="M480 150L430 150L430 100L480 100L480 150Z" fill="currentColor" />
 		</g>
 	{/if}
 </svg>
