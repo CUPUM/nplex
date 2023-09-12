9bdc069241d0e01ac527faaf15b301afb517ef29
Emmanuel <emmbema@gmail.com>
2023-09-13 01:06:45 +0300

    Start implementing authorization query helpers as where clause filters

---
+++ src/lib/db/authorizations.ts
@@ -0,0 +1,32 @@
+import { and, eq, exists, or } from 'drizzle-orm';
+import type { Session } from 'lucia';
+import { dbhttp } from './db.server';
+import { users } from './schema/personal';
+import { projects, projectsUsers } from './schema/public';
+
+export function authorizeUserUpdate(session: Session) {
+	return eq(users.id, session.user.id);
+}
+
+export function authorizePublicProjectSelect(session: Session) {
+	return session;
+}
+
+export function authorizeProjectUpdate(session: Session) {
+	return or(
+		eq(projects.createdById, session.user.id),
+		exists(
+			dbhttp
+				.select()
+				.from(projectsUsers)
+				.where(
+					and(eq(projectsUsers.projectId, projects.id), eq(projectsUsers.userId, session.user.id))
+				)
+				.limit(1)
+		)
+	);
+}
+
+export function authorizeProjectPublication(session: Session) {
+	return session;
+}
