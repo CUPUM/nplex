abbb2147eeb628c12661dafb8e66bf239d28a71e
Emmanuel <emmbema@gmail.com>
2023-10-07 02:10:17 -0400

    Add favicon, add placeholder montreal logo

--- src/lib/components/footer.svelte
+++ src/lib/components/footer.svelte
@@ -1,10 +1,23 @@
 <script lang="ts">
 </script>
 
-<footer>footer goes vroom.</footer>
+<footer>
+	<div class="inner"></div>
+</footer>
 
 <style lang="postcss">
 	footer {
+		font-size: var(--size-sm);
 		padding: 2rem;
+		background-color: color-mix(in srgb, var(--color-neutral-500) 10%, transparent);
+		border-radius: var(--radius-lg);
+		margin: 1rem 0.75rem;
+		max-width: 100%;
+		:global(:--setout-normal) & {
+			max-width: var(--width-lg);
+		}
+	}
+
+	.inner {
 	}
 </style>
