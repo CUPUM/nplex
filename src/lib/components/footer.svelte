100366696910a6df85d8de1ef30b4f3688b018a8
Emmanuel <emmbema@gmail.com>
2023-10-07 22:28:54 -0400

    Add temp logos

--- src/lib/components/footer.svelte
+++ src/lib/components/footer.svelte
@@ -1,12 +1,31 @@
 <script lang="ts">
+	import { page } from '$app/stores';
+	import { LOCALES } from '$lib/i18n/constants';
+	import CupumUdem from './partners-logos/CupumUdem.svelte';
 	import Montreal from './partners-logos/Montreal.svelte';
 </script>
 
 <footer>
 	<div class="inner">
-		<a href="https://montreal.ca/unites/bureau-du-design" class="footer-link">
-			<Montreal style="height: 2em" />
-		</a>
+		<ul class="logos">
+			<li>
+				<a href="https://www.unesco-paysage.umontreal.ca/{$page.data.locale}/" class="footer-link">
+					<CupumUdem style="width: 10em; height: auto" />
+				</a>
+			</li>
+			<hr class="hr" />
+			<li>
+				<a
+					href="https://montreal.ca/unites/bureau-du-design{$page.data.locale === LOCALES.ENGLISH
+						? '/en'
+						: ''}"
+					class="footer-link"
+				>
+					<Montreal style="width: 10em; height: auto" />
+				</a>
+			</li>
+		</ul>
+		<ul class="links"></ul>
 	</div>
 </footer>
 
@@ -29,12 +48,34 @@
 		padding: 2rem;
 		width: 100%;
 		max-width: var(--width-lg);
+		align-self: center;
+	}
+
+	.logos {
+		display: flex;
+		flex-direction: row;
+		justify-content: center;
+		align-items: center;
+		gap: 3rem;
+		font-size: var(--size-xs);
+
+		hr {
+			all: unset;
+			width: 1px;
+			flex: none;
+			align-self: stretch;
+			background-color: var(--color-neutral-500);
+			opacity: 0.2;
+		}
 	}
 
 	.footer-link {
 		transition: all var(--duration-medium) ease-out;
 		&:hover {
-			color: var(--color-primary-500);
+			color: var(--color-primary-700);
+			:global(:--dark) & {
+				color: var(--color-primary-500);
+			}
 		}
 	}
 </style>
