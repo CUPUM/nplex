46ef72ac901b27085ff4cc43925a4e3b7f84173f
Emmanuel <emmbema@gmail.com>
2023-10-09 09:43:19 -0400

    Try debugging broken form

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,6 @@
 import { preprocessMeltUI } from '@melt-ui/pp';
 import adapter from '@sveltejs/adapter-vercel';
+// import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 import sequence from 'svelte-sequential-preprocessor';
 
