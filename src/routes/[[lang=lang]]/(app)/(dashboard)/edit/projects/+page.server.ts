a134745148553f6a7968aab89610d4e327f69c08
Emmanuel <emmbema@gmail.com>
2023-11-30 13:13:48 -0500

    Work on editable projects browser

--- src/routes/[[lang=lang]]/(app)/(dashboard)/edit/projects/+page.server.ts
+++ src/routes/[[lang=lang]]/(app)/(dashboard)/edit/projects/+page.server.ts
@@ -1,31 +1,28 @@
 import { withAuth } from '$lib/auth/guard.server';
+import { authorizeProjectUpdate } from '$lib/db/authorization.server';
 import { dbpool } from '$lib/db/db.server';
-import { projectsTranslations } from '$lib/db/schema/public';
-import { ilike } from 'drizzle-orm';
-('$lib/db/schema/public');
+import { projects, projectsTranslations } from '$lib/db/schema/public';
+import { getSubqueryColumns } from '$lib/db/utils.server';
+import { LOAD_DEPENDENCIES } from '$lib/utils/constants';
+import { and, eq, getTableColumns, ilike } from 'drizzle-orm';
 
 export const load = async (event) => {
 	const session = await withAuth(event);
+	event.depends(LOAD_DEPENDENCIES.Lang);
 	const search = event.url.searchParams.get('search') ?? '';
 	const match = dbpool
 		.select()
 		.from(projectsTranslations)
-		.where(ilike(projectsTranslations.title, `%${search}%`))
+		.where(search ? ilike(projectsTranslations.title, `%${search}%`) : undefined)
 		.as('match');
-	// const searchProjects = dbpool.selectDistinctOn
-	// withTranslation(event, projects, projectsTranslations, {
-	// 	field: (t) => t.id,
-	// 	reference: (tt) => tt.id,
-	// });
-	// const filteredProjects = await dbpool
-	// 	.selectDistinctOn([projects.id], { id: projects.id, title: projectsTranslations.title })
-	// 	.from(projects)
-	// 	.leftJoin(projectsTranslations, eq(projects.id, projectsTranslations.id))
-	// 	.where(and(authorizeProjectUpdate(session), ilike(projectsTranslations.title, `%${q ?? ''}%`)));
-
-	// withTranslation();
-	// console.log(filteredProjects);
-	// return { filteredProjects };
+	const { title, summary } = getSubqueryColumns(match);
+	const { id, bannerId } = getTableColumns(projects);
+	const searchProjects = dbpool
+		.select({ id, bannerId, title, summary })
+		.from(projects)
+		.rightJoin(match, eq(projects.id, match.id))
+		.where(and(authorizeProjectUpdate(session), eq(match.lang, event.locals.lang)));
+	return { searchProjects, search };
 };
 
 export const actions = {};
--- src/routes/[[lang=lang]]/(app)/(dashboard)/edit/projects/+page.svelte
+++ src/routes/[[lang=lang]]/(app)/(dashboard)/edit/projects/+page.svelte
@@ -1,70 +1,156 @@
 <script lang="ts">
+	import { page } from '$app/stores';
 	import * as m from '$i18n/messages';
+	import { ripple } from '$lib/actions/ripple';
 	import LangKey, { langKey } from '$lib/components/LangKey.svelte';
 	import { link } from '$lib/i18n/link';
-	import { FilePlus, Search } from 'lucide-svelte';
-	import { cubicOut } from 'svelte/easing';
-	import { fade, fly } from 'svelte/transition';
+	import { FilePlus, Search, X } from 'lucide-svelte';
+	import { flip } from 'svelte/animate';
+	import { cubicOut, expoOut } from 'svelte/easing';
+	import { fly, scale } from 'svelte/transition';
 
 	export let data;
 </script>
 
-<form method="GET" data-sveltekit-keepfocus data-sveltekit-replacestate data-sveltekit-noscroll>
-	<section id="projects-actions">
-		<!-- <header>
-			<h1 class="h2">
-				<LangKey>{m.project_edit()}</LangKey>
-			</h1>
-		</header> -->
-		<!-- svelte-ignore a11y-missing-attribute -->
-		<a
-			{...$link(`/new/project`)}
-			class="button cta"
-			in:fly|global={{ y: 8, easing: cubicOut, duration: 450, delay: 250 }}
-		>
-			<LangKey>{@html m.project_create()}</LangKey>
-			<FilePlus class="button-icon" />
-		</a>
-		<span class="dim" in:fade|global={{ delay: 500, duration: 1000 }}>
-			<LangKey>{m.or()}</LangKey>
-		</span>
-		<fieldset
+<article>
+	<header>
+		<h1 class="h2">
+			<LangKey>{m.project_edit()}</LangKey>
+		</h1>
+	</header>
+	<section>
+		<form
+			method="GET"
+			data-sveltekit-keepfocu
+			data-sveltekit-replacestate
+			data-sveltekit-noscroll
 			class="input-group bg-blur"
-			in:fly|global={{ y: -8, easing: cubicOut, duration: 450, delay: 250 }}
+			in:fly|global={{ y: 6, easing: cubicOut, duration: 450 }}
 		>
-			<input type="search" name="search" placeholder={$langKey(m.project_find())} class="input" />
+			<input
+				type="search"
+				name="search"
+				placeholder={$langKey(m.project_find())}
+				class="input"
+				value={data.search}
+			/>
 			<div class="input-peer">
+				{#if $page.url.searchParams.get('search')}
+					<a
+						transition:scale={{ duration: 250, start: 0.5, opacity: 0, easing: expoOut }}
+						{...$link('/edit/projects')}
+						class="button ghost square"
+					>
+						<X />
+					</a>
+				{/if}
 				<button class="button cta square" type="submit">
 					<Search class="button-icon" />
 				</button>
 			</div>
-		</fieldset>
-	</section>
-	<ul>
-		<!-- {#each data.filteredProjects as project}
+		</form>
+		<ul>
 			<li>
-				<a {...$link(`/edit/projects/${project.id}`)}>
-					{project.title}
+				<!-- svelte-ignore a11y-missing-attribute -->
+				<a
+					{...$link(`/new/project`)}
+					class="card"
+					use:ripple
+					in:fly|global={{ y: -6, easing: cubicOut, duration: 350 }}
+				>
+					<div class="button cta rounded">
+						<LangKey>{@html m.project_create()}</LangKey>
+						<FilePlus class="button-icon" />
+					</div>
 				</a>
 			</li>
-		{/each} -->
-	</ul>
-</form>
+			{#each data.searchProjects as project, i (project.id)}
+				<li
+					animate:flip={{ duration: (l) => 150 + l / 100 }}
+					in:fly|global={{ y: -6, duration: 350, easing: expoOut, delay: i * 25, opacity: 0 }}
+				>
+					<a class="card" {...$link(`/edit/projects/${project.id}`)} use:ripple>
+						{#if project.title}
+							<span class="title">{project.title}</span>
+						{:else}
+							<span class="no-title"><LangKey>{m.project_untitled()}</LangKey></span>
+						{/if}
+					</a>
+				</li>
+			{/each}
+		</ul>
+	</section>
+</article>
 
 <style lang="postcss">
-	form {
+	article {
 		grid-column: 1/-1;
 		position: relative;
 		width: 100%;
-		display: grid;
-		grid-template-columns: 1fr 1fr;
+		display: flex;
+		flex-direction: column;
 		min-height: calc(100vh - var(--navbar-height) - 2 * var(--base-gutter));
 		min-height: calc(100svh - var(--navbar-height) - 2 * var(--base-gutter));
 	}
 
-	.dim {
-		font-size: var(--size-sm);
-		opacity: var(--opacity-dim);
+	form {
+		/* --input-radius: var(--radius-sm); */
+		position: sticky;
+		top: var(--sticky-top);
+		z-index: 1;
+	}
+
+	section {
+		display: flex;
+		flex-direction: column;
+		gap: 2rem;
+		padding: 2rem;
+		border-radius: var(--radius-lg);
+		background: var(--dashboard-bg);
+		flex: 1;
+		/* border: var(--base-border-width) solid var(--base-border-color); */
+	}
+
+	ul {
+		display: grid;
+		grid-template-columns: repeat(auto-fill, minmax(225px, 1fr));
+		justify-items: center;
+		align-items: flex-start;
+		flex-direction: row;
+		gap: var(--base-gutter);
+		flex-wrap: wrap;
+	}
+
+	li {
+		position: relative;
+		flex: none;
+		width: 100%;
+	}
+
+	.card {
+		border-radius: var(--radius-md);
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		font-size: var(--size-xs);
+		width: 100%;
+		aspect-ratio: 3/4;
+		padding: 2rem;
+		background: var(--color-neutral-200);
+
+		:global(:--dark) & {
+			background: var(--color-neutral-900);
+		}
+
+		.title {
+			font-size: var(--size-lg);
+		}
+
+		.no-title {
+			opacity: var(--opacity-dimmer);
+			font-style: italic;
+			font-weight: 300;
+		}
 	}
 
 	header {
