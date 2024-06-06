4276ae0ec76d7b0787eff97a9ff850fdb883d66e
Emmanuel Beaudry Marchand <emmbema@gmail.com>
2024-06-06 11:26:30 -0400

    Scaffold avatar primitive

---
+++ src/lib/components/primitives/avatar.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import type { HTMLAttributes } from 'svelte/elements';
+	import { cn } from '../utilities';
+
+	let {
+		id,
+		email,
+		imageUrl,
+		class: className,
+	}: {
+		id: string;
+		email?: string | null;
+		imageUrl?: string | null;
+	} & HTMLAttributes<HTMLElement> = $props();
+
+	let char = $derived(email ? email.charAt(0) : id.charAt(0));
+</script>
+
+<figure
+	class={cn(
+		'bg-card text-md pointer-events-none relative inset-0 flex aspect-square items-center justify-center font-bold uppercase',
+		className
+	)}
+>
+	<span class="absolute leading-none">
+		{char}
+	</span>
+</figure>
