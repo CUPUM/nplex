2e92a20d6f77590af0c5100af9dbd3c1d04c53dd
Emmanuel Beaudry Marchand <emmbema@gmail.com>
2024-06-06 11:11:12 -0400

    Add basic portal action

--- src/lib/actions/portal.ts
+++ src/lib/actions/portal.ts
@@ -1,9 +1,15 @@
-export function portal(node: Node, { target = document.body }: { target: HTMLElement }) {
-	target.appendChild(node);
-
+export function portal(
+	node: Node,
+	{ target = document.body, anchor }: { target?: HTMLElement; anchor?: HTMLElement } = {}
+) {
+	if (anchor) {
+		target.insertBefore(node, anchor);
+	} else {
+		target.appendChild(node);
+	}
 	return {
 		destroy() {
-			target.removeChild(node);
+			// target.removeChild(node);
 		},
 	};
 }
