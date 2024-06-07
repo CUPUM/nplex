6aac2f446fff8138c7ceea7d1a8355675d34332f
Emmanuel Beaudry Marchand <emmbema@gmail.com>
2024-06-07 18:52:48 -0400

    Update i18n crud modules

---
+++ src/lib/crud/queries/i18n.ts
@@ -0,0 +1,48 @@
+import { languageTag, type AvailableLanguageTag } from '$i18n/runtime';
+import { LANG_COLUMN_NAME } from '$lib/db/constants';
+import type { LangColumn } from '$lib/db/helpers.server';
+import { languages } from '$lib/db/schema/i18n.server';
+import { SQL, and, eq, type AnyTable, type ColumnsSelection, type Subquery } from 'drizzle-orm';
+import { $true } from 'drizzle-orm-helpers';
+import { jsonBuildObject, jsonObjectAgg } from 'drizzle-orm-helpers/pg';
+import type { PgSelect, TableConfig } from 'drizzle-orm/pg-core';
+
+export function aggTranslations<T extends ColumnsSelection>(fields: T) {
+	return {
+		translations: jsonObjectAgg(languages[LANG_COLUMN_NAME], jsonBuildObject(fields)),
+	};
+}
+
+/**
+ * Join all available language tag's translations as aggergated fields.
+ */
+export function joinTranslations<
+	TSelect extends PgSelect,
+	TTranslations extends
+		| (AnyTable<TableConfig> & LangColumn)
+		| (Subquery<string, Record<string, unknown>> & LangColumn),
+>(select: TSelect, translations: TTranslations, on: SQL) {
+	return select
+		.leftJoin(languages, $true)
+		.leftJoin(
+			translations,
+			and(on, eq(languages[LANG_COLUMN_NAME], translations[LANG_COLUMN_NAME]))
+		);
+}
+
+/**
+ * Join a single language's fields directly into the data.
+ */
+export function joinTranslation<
+	TSelect extends PgSelect,
+	TTranslations extends
+		| (AnyTable<TableConfig> & LangColumn)
+		| (Subquery<string, Record<string, unknown>> & LangColumn),
+>(
+	select: TSelect,
+	translations: TTranslations,
+	on: SQL,
+	lang: AvailableLanguageTag = languageTag()
+) {
+	// to do
+}
--- src/lib/crud/validation/i18n.ts
+++ src/lib/crud/validation/i18n.ts
@@ -2,25 +2,37 @@ import type { AvailableLanguageTag } from '$i18n/runtime';
 import { availableLanguageTags, isAvailableLanguageTag } from '$i18n/runtime';
 import { LANG_COLUMN_NAME } from '$lib/db/constants';
 import { ZodObject, z, type ZodTypeAny } from 'zod';
-import { strictRecord } from './utils';
 
 export const langSchema = z.custom<AvailableLanguageTag>(isAvailableLanguageTag);
 
 export type LangSchema = typeof langSchema;
 
 /**
- * Extend the insert schema of a given ressource table with its corresponding translations. The
- * resulting schema should be isomorphic with {@link withTranslations}.
+ * Needed to extend schemas produced by drizzle-zod since custom columns are rendered as ZodAny.
+ */
+export const LANG_COLUMN_SCHEMA = {
+	[LANG_COLUMN_NAME]: langSchema,
+};
+
+export type LangColumnSchema = typeof LANG_COLUMN_SCHEMA;
+
+/**
+ * Extend the insert schema of a given ressource table with its corresponding translations.
  */
 export function withTranslationsSchema<
 	T extends Record<string, ZodTypeAny>,
-	TT extends Record<string, ZodTypeAny> & { lang: LangSchema },
+	TT extends Record<string, ZodTypeAny> & LangColumnSchema,
 >(schema: ZodObject<T>, translationSchema: ZodObject<TT>) {
-	return schema.extend({
-		translations: strictRecord(availableLanguageTags, (lang) =>
-			translationSchema
-				.omit({ [LANG_COLUMN_NAME]: true })
-				.extend({ [LANG_COLUMN_NAME]: translationSchema.shape[LANG_COLUMN_NAME].default(lang) })
-		),
-	});
+	const translations = z.object(
+		availableLanguageTags.reduce(
+			(acc, lang) => {
+				acc[lang] = translationSchema.merge(
+					z.object({ [LANG_COLUMN_NAME]: langSchema.default(lang) })
+				) as never;
+				return acc;
+			},
+			{} as Record<AvailableLanguageTag, ZodObject<TT>>
+		)
+	);
+	return schema.extend({ translations });
 }
