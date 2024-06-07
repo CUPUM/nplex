e1b67bef5239fefba36d59c94a6e153f019b72b1
Emmanuel Beaudry Marchand <emmbema@gmail.com>
2024-06-07 15:28:31 -0400

    Update user dashboard layout

--- src/routes/[[lang=lang]]/(dashboard)/i/+layout.svelte
+++ src/routes/[[lang=lang]]/(dashboard)/i/+layout.svelte
@@ -4,6 +4,7 @@
 	import DashboardSidebarMenu from '$lib/components/patterns/dashboard-sidebar-menu.svelte';
 	import { getDashboardContext } from '$lib/components/patterns/dashboard.svelte';
 	import { linkAttributes } from '$lib/components/primitives/link.svelte';
+	import { ripple } from '$lib/components/primitives/ripple.svelte';
 	import {
 		ArrowRight,
 		FolderHeart,
@@ -29,40 +30,41 @@
 		{#snippet legend()}
 			{m.account_nav_title()}
 		{/snippet}
-		<a class="button button-nav" {...linkAttributes(`/i`)}>
-			{m.account_nav_home()}
+		<a class="button button-nav" use:ripple {...linkAttributes(`/i`)}>
 			<Home />
+			{m.account_nav_home()}
 		</a>
 		<a
 			class="button button-nav"
+			use:ripple
 			{...linkAttributes(`/users/${$page.data?.user?.id}`)}
 			aria-disabled
 		>
-			{m.account_nav_profile()}
 			<SquareUserRoundIcon />
+			{m.account_nav_profile()}
 		</a>
-		<a class="button button-nav" {...linkAttributes(`/i/collections`)} aria-disabled>
-			{m.account_nav_collections()}
+		<a class="button button-nav" use:ripple {...linkAttributes(`/i/collections`)} aria-disabled>
 			<Folders />
+			{m.account_nav_collections()}
 		</a>
-		<a class="button button-nav" {...linkAttributes(`/i/likes`)} aria-disabled>
-			{m.account_nav_likes()}
+		<a class="button button-nav" use:ripple {...linkAttributes(`/i/likes`)} aria-disabled>
 			<FolderHeart />
+			{m.account_nav_likes()}
 		</a>
-		<a class="button button-nav" {...linkAttributes(`/i/settings`)}>
-			{m.account_nav_settings()}
+		<a class="button button-nav" use:ripple {...linkAttributes(`/i/settings`)}>
 			<Settings />
+			{m.account_nav_settings()}
 		</a>
 	</DashboardSidebarMenu>
 	<DashboardSidebarMenu>
 		{#snippet legend()}
 			{m.account_nav_editor_title()}
 		{/snippet}
-		<a class="button button-nav" {...linkAttributes(`/edit/projects`)}>
+		<a class="button button-nav" use:ripple {...linkAttributes(`/edit/projects`)}>
 			{m.projects()}
 			<ArrowRight />
 		</a>
-		<a class="button button-nav" {...linkAttributes(`/edit/organizations`)}>
+		<a class="button button-nav" use:ripple {...linkAttributes(`/edit/organizations`)}>
 			{m.organizations()}
 			<ArrowRight />
 		</a>
