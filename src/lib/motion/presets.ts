c63740d26c31e70363be8e7dac6ece01b053cb6f
Emmanuel Beaudry Marchand <emmbema@gmail.com>
2024-06-12 14:18:26 -0400

    Export crossfade presets as objects instead of tuples for clarity

--- src/lib/motion/presets.ts
+++ src/lib/motion/presets.ts
@@ -3,7 +3,15 @@
 import { cubicInOut, elasticOut, expoInOut, expoOut } from 'svelte/easing';
 import { crossfade, scale } from 'svelte/transition';
 
-export const switchCrossfade = crossfade({
+function createCrossfadePreset<T extends Parameters<typeof crossfade>>(...params: T) {
+	const preset = crossfade(params);
+	return {
+		send: preset[0],
+		receive: preset[1],
+	};
+}
+
+export const switchCrossfade = createCrossfadePreset({
 	duration(l) {
 		return 150 + l / 10;
 	},
@@ -13,21 +21,17 @@ export const switchCrossfade = crossfade({
 	},
 });
 
-export const toggleCrossfade = crossfade({
+export const toggleCrossfade = createCrossfadePreset({
 	duration: 250,
 	fallback(node /*params, intro */) {
 		return scale(node, { start: 0.5, duration: 500, easing: elasticOut });
 	},
 });
 
-export const galleryImageCrossfade = crossfade({
+export const galleryImageCrossfade = createCrossfadePreset({
 	duration: 250,
 	easing: expoInOut,
 	fallback(node) {
 		return scale(node, { start: 0.9, duration: 150, easing: expoOut });
 	},
 });
-
-// export const dialogTransition =
-
-// export const dropdownTransition =
