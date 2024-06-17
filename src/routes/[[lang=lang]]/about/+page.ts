6c25fe9167e0fbee1b92c82fa6347fd824bfd3f1
Emmanuel Beaudry Marchand <emmbema@gmail.com>
2024-06-17 15:00:46 -0400

    Specify entries for prerendering

--- src/routes/[[lang=lang]]/about/+page.ts
+++ src/routes/[[lang=lang]]/about/+page.ts
@@ -1,13 +1,18 @@
+import { availableLanguageTags, sourceLanguageTag } from '$i18n/runtime';
 import { z } from 'zod';
 
 export const prerender = true;
+
+export function entries() {
+	return availableLanguageTags.map((lang) => ({ lang }));
+}
 
 const metadataSchema = z.object({
 	title: z.string(),
 });
 
 export const load = async (event) => {
-	const md = await import(`./${event.params.lang}.mdx`);
+	const md = await import(`./${event.params.lang ?? sourceLanguageTag}.mdx`);
 	const metadata = metadataSchema.parse(md.metadata || {});
 	const content = md.default;
 	return {
--- src/routes/[[lang=lang]]/guides/+page.ts
+++ src/routes/[[lang=lang]]/guides/+page.ts
@@ -1,14 +1,19 @@
+import { availableLanguageTags, sourceLanguageTag } from '$i18n/runtime';
 import { headingsMetadataSchema } from '$lib/markdown/validation';
 import { z } from 'zod';
 
 export const prerender = true;
+
+export function entries() {
+	return availableLanguageTags.map((lang) => ({ lang }));
+}
 
 const metadataSchema = headingsMetadataSchema.extend({
 	title: z.string(),
 });
 
 export const load = async (event) => {
-	const md = await import(`./${event.params.lang}.mdx`);
+	const md = await import(`./${event.params.lang ?? sourceLanguageTag}.mdx`);
 	const metadata = metadataSchema.parse(md.metadata || {});
 	const content = md.default;
 	return {
