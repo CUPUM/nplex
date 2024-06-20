d373650d6a306f7c95ca8d944887af3bfb35de82
Emmanuel Beaudry Marchand <emmbema@gmail.com>
2024-06-20 16:21:46 -0400

    Create fallback text component to simplify markup for null values

---
+++ src/lib/components/primitives/optional-text.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import * as m from '$i18n/messages';
+	import type { HTMLAttributes } from 'svelte/elements';
+	import { cn } from '../utilities';
+
+	let {
+		text,
+		fallback,
+		class: className,
+		...spanProps
+	}: { text?: string | null; fallback?: string } & HTMLAttributes<HTMLSpanElement> = $props();
+</script>
+
+{#if text}
+	{@html text}
+{:else}
+	<span class={cn('opacity-dim font-thin italic', className)} {...spanProps}>
+		{@html fallback ?? m.not_defined()}
+	</span>
+{/if}
